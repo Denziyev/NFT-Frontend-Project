8dc50e31241b84ee38c132d9eb97abd219aae914
İlkin <ilkinhd@code.edu.az>
2023-05-31 21:01:56 +0400

    browse categories section is added

--- Javascript/homePage.js
+++ Javascript/homePage.js
@@ -23,22 +23,60 @@ creator11.src = "../assets/image/avatars/avil/Avatar11.png";
 let creator12 = document.createElement("img");
 creator12.src = "../assets/image/avatars/avil/Avatar12.png";
 
-const creatorcards=document.querySelector(".creatorcards")
+const creatorcards = document.querySelector(".creatorcards")
 let creatorimages = [creator1, creator2, creator3, creator4, creator5, creator6, creator7, creator8, creator9, creator10, creator11, creator12];
-let creatornames=['Keepitreal','DigiLab','GravityOne','Juanie','BlueWhale','Mr Fox','Shroomie','robotica','RustyRobot','Animakid','Dotgu','Ghiblier'];
+let creatornames = ['Keepitreal', 'DigiLab', 'GravityOne', 'Juanie', 'BlueWhale', 'Mr Fox', 'Shroomie', 'robotica', 'RustyRobot', 'Animakid', 'Dotgu', 'Ghiblier'];
 
 
 
 for (let i = 1; i <= creatorimages.length; i++) {
     const newcarddiv = document.createElement("div");
+    newcarddiv.classList.add("newcard");
     newcarddiv.innerHTML = `
-     
-      <img src=${creatorimages[i-1].src}>
+      <div>${i}</div>
+      <img src=${creatorimages[i - 1].src}>
       <div class="creatorcardbody">
-      <div>${creatornames[i-1]}</div>
+      <div>${creatornames[i - 1]}</div>
       <div><div>Total Sales:</div><div>34.53 ETH</div></div>
       </div>
     `;
-    newcarddiv.classList.add("newcard");
+    
     creatorcards.append(newcarddiv);
+}
+
+
+//-----------------------------------------------------
+
+
+const browse1=document.createElement("img");
+browse1.src="../assets/image/browseCategories/browse1.png";
+const browse2=document.createElement("img");
+browse2.src="../assets/image/browseCategories/browse2.png";
+const browse3=document.createElement("img");
+browse3.src="../assets/image/browseCategories/browse3.png";
+const browse4=document.createElement("img");
+browse4.src="../assets/image/browseCategories/browse4.png";
+const browse5=document.createElement("img");
+browse5.src="../assets/image/browseCategories/browse5.png";
+const browse6=document.createElement("img");
+browse6.src="../assets/image/browseCategories/browse6.png";
+const browse7=document.createElement("img");
+browse7.src="../assets/image/browseCategories/browse7.png";
+const browse8=document.createElement("img");
+browse8.src="../assets/image/browseCategories/browse8.png";
+
+const browsecards = document.querySelector(".browsecards");
+let browseimages = [browse1, browse2, browse3, browse4, browse5, browse6, browse7, browse8];
+let browsenames = ['Art', 'Collectibles', 'Music', 'Photography', 'Video', 'Utility', 'Sport', 'Virtual Worlds'];
+
+for(let i=0;i<browseimages.length;i++){
+    const newbrowsecarddiv = document.createElement("div");
+    newbrowsecarddiv.className="newbrowsecard";
+    newbrowsecarddiv.innerHTML=`
+    <img src=${browseimages[i].src}>
+    <div>
+    <p>${browsenames[i]}</p>
+    <div>
+    `;
+    browsecards.append(newbrowsecarddiv);
 }
